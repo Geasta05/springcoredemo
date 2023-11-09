773f48381b084ca3a13b649a761e6b8c99a17be8
Scott Smith <ssm2876@wgu.edu>
2023-11-09 10:01:54 -0800

    Added "PostConstruct" and "PreDestroy" annotations and applicable methods ("doMyStartup", "doMyCleanup")

--- src/main/java/geasta05/springcoredemo/common/CricketCoach.java
+++ src/main/java/geasta05/springcoredemo/common/CricketCoach.java
@@ -1,12 +1,25 @@
 package geasta05.springcoredemo.common;
 
 import geasta05.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
+    @PostConstruct
+    public void doMyStartup() {
+        System.out.println("In doMyStartup(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanup() {
+        System.out.println("In doMyCleanup(): " + getClass().getSimpleName());
+    }
+
 
     @Override
     public String getDailyWorkout() {
