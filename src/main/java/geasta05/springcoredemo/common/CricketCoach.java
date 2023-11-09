c0c8ad660d525f8c89719c418b8760a02695e6e6
Scott Smith <ssm2876@wgu.edu>
2023-11-09 09:49:29 -0800

    Changed "CricketCoach" bean scope from Singleton to Prototype.

--- src/main/java/geasta05/springcoredemo/common/CricketCoach.java
+++ src/main/java/geasta05/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,12 @@
 package geasta05.springcoredemo.common;
 
 import geasta05.springcoredemo.common.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     @Override
