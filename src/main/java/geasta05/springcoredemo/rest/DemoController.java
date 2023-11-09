76b4837fce219a1dd898a3f8fd43148b55db822f
Scott Smith <ssm2876@wgu.edu>
2023-11-08 23:01:26 -0800

    Setup a qualifier for DemoController to select BaseballCoach

--- src/main/java/geasta05/springcoredemo/rest/DemoController.java
+++ src/main/java/geasta05/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package geasta05.springcoredemo.rest;
 
 import geasta05.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class DemoController {
 
     //with @Autowired annotation, any method can act as a setter injector (not just setters)
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
