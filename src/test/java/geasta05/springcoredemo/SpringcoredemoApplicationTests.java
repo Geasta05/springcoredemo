edaeff0a57a2795efc3d4d3266f14f4f9522fcea
Scott Smith <ssm2876@wgu.edu>
2023-11-08 19:58:58 -0800

    Initial Project Add

---
+++ src/test/java/geasta05/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package geasta05.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
